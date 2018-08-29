4573d686d67a02c005228ee92d3e3be58b69b313
Kaustubh Deshpande <kaustubhdeshpande@Kaustubhs-MacBook-Pro.local>
2018-08-29 17:10:47 -0500

    home page

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import Particles from "react-particles-js";
 import "./style.css";
-// import MainLogo from "../../images/MainLogo.png";
+import FloatingActionButtons from "../Buttons/Buttons";
+import MainLogo from "../../../src/LogoMakr_7PRQtL.png";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Icon from "@material-ui/core/Icon";
+import DeleteIcon from "@material-ui/icons/Delete";
+import NavigationIcon from "@material-ui/icons/Navigation";
+
 // other pages
 // import Description from "./../Description/Description";
 // import Contact from "./../Contact/Contact";
@@ -14,64 +21,84 @@ class landing extends Component {
     return (
       <div id="top">
         <div className="landing-body">
-          <div className="landing-title">
-            <div className="particlesHider">
-              <Particles
-                className="particlesHider"
-                params={{
-                  particles: {
-                    number: {
-                      value: 160,
-                      density: {
-                        enable: false
-                      }
-                    },
-                    size: {
-                      value: 3,
-                      random: true,
-                      anim: {
-                        speed: 4,
-                        size_min: 0.3
-                      }
-                    },
-                    line_linked: {
-                      enable: false
-                    },
-                    move: {
-                      bounce: false,
-                      random: true,
-                      speed: 1,
-                      direction: "random",
-                      out_mode: "out"
-                    }
-                  },
-                  interactivity: {
-                    detect_on: "canvas",
-                    events: {
-                      onhover: {
-                        enable: true,
-                        mode: "grab"
+          <div className="container">
+            <div className="landing-title">
+              <div className="particlesHider">
+                <Particles
+                  className="particlesHider"
+                  params={{
+                    particles: {
+                      number: {
+                        value: 50,
+                        density: {
+                          enable: false
+                        }
                       },
-                      resize: true
+                      color: {
+                        value: ["#ff524e", "#50a3e3", "#BA8F7F"]
+                      },
+                      size: {
+                        value: 4,
+                        random: true,
+                        anim: {
+                          speed: 4,
+                          size_min: 0.3
+                        }
+                      },
+                      line_linked: {
+                        enable: false
+                      },
+                      move: {
+                        bounce: false,
+                        random: true,
+                        speed: 6,
+                        direction: "top-right",
+                        out_mode: "out"
+                      }
                     },
-                    modes: {
-                      grab: {
-                        distance: 140
+                    interactivity: {
+                      detect_on: "canvas",
+                      events: {
+                        onhover: {
+                          enable: true,
+                          mode: "grab"
+                        },
+                        resize: true
+                      },
+                      modes: {
+                        grab: {
+                          distance: 140
+                        }
                       }
                     }
-                  }
-                }}
-              />
-            </div>
-            <div className="app-intro">
-              {/* <img className="centered" src={MainLogo} /> */}
-              <button
-                id="connect-button"
-                className="btn yellow"
-                onClick={this.goLogin}
-              >
-                Connect
-              </button>
+                  }}
+                />
+              </div>
+              <div className="app-intro">
+                {/* <img className="centered" src={MainLogo} /> */}
+                <div
+                  style={{
+                    fontFamily: "Playfair Display ,serif",
+                    color: "white",
+                    fontSize: "55px"
+                  }}
+                >
+                  S o c i a L i s t a
+                </div>
+                <div
+                  id="slogan"
+                  style={{
+                    fontFamily: "Playfair Display ,serif",
+                    color: "white",
+                    fontSize: "20px"
+                  }}
+                >
+                  Be Simple, Be Social
+                </div>
+                <div id="connect-button">
+                  <FloatingActionButtons />
+                </div>
+              </div>
             </div>
           </div>
           {/* <Features />
