ca474d1222642c3cf6e8efc9622abdd266191fa1
Abdul Kanjo <Abdul.kanjo@gmail.com>
2018-09-06 09:28:19 -0500

    fixed stories issue with a new endpoint

--- src/components/Stories/Stories.js
+++ src/components/Stories/Stories.js
@@ -1,115 +1,141 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import MobileStepper from "@material-ui/core/MobileStepper";
-import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import Modal from "@material-ui/core/Modal";
 import Button from "@material-ui/core/Button";
-import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
-import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import TextMobileStepper from "./StoryDisplay";
+import axios from "axios";
+import Avatar from "@material-ui/core/Avatar";
 
-let tutorialSteps = [];
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)"
+  };
+}
+
 const styles = theme => ({
-  root: {
-    maxWidth: 400,
-    flexGrow: 1
+  paper: {
+    position: "absolute",
+    width: theme.spacing.unit * 50,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4
   },
-  header: {
+  row: {
     display: "flex",
-    alignItems: "center",
-    height: 50,
-    paddingLeft: theme.spacing.unit * 4,
-    marginBottom: 20,
-    backgroundColor: theme.palette.background.default
+    justifyContent: "center"
   },
-  img: {
-    height: 255,
-    maxWidth: 400,
-    overflow: "hidden",
-    width: "100%"
+  avatar: {
+    margin: 10
   }
 });
 
-class TextMobileStepper extends React.Component {
+class SimpleModal extends React.Component {
   constructor() {
     super();
     this.state = {
-      activeStep: 0,
-      stories: []
+      open: false,
+      stories: [],
+      currentUser: "",
+      friends: [],
+      loadingStories: true,
+      loadingFriends: true
     };
+    this.getStories = this.getStories.bind(this);
+    this.getFriends = this.getFriends.bind(this);
   }
 
-  handleNext = () => {
-    this.setState(prevState => ({
-      activeStep: prevState.activeStep + 1
-    }));
+  handleOpen = () => {
+    this.setState({ open: true });
   };
 
-  handleBack = () => {
-    this.setState(prevState => ({
-      activeStep: prevState.activeStep - 1
-    }));
+  handleClose = () => {
+    this.setState({ open: false });
   };
+
+  componentDidMount() {
+    this.getFriends(this.props.currentUser.auth_id);
+    this.getStories(this.props.currentUser.auth_id);
+  }
+
+  getStories(auth_id) {
+    axios.get(`/api/getstories/${auth_id}`).then(res => {
+      this.setState({ stories: res.data, loadingStories: false });
+    });
+  }
+
+  getFriends(auth_id) {
+    axios.get(`/api/getlistOffriends/${auth_id}`).then(res => {
+      this.setState({ friends: res.data, loadingFriends: false });
+    });
+  }
 
   render() {
-    const { classes, theme } = this.props;
-    const { activeStep } = this.state;
+    console.log(this.state.friends);
 
-    const maxSteps = tutorialSteps.length;
-    tutorialSteps = this.props.mappedStories;
+    const { classes } = this.props;
+
+    if (this.state.loadingStories || this.state.loadingFriends) {
+      return null;
+    }
+
+    let mappedStories = this.state.stories.map((story, i) => {
+      // console.log(story);
+      return {
+        label: story.title,
+        imgPath: story.img_url
+      };
+    });
+
+    let mappedAvatars = this.state.friends.map((avatar, i) => {
+      return (
+        <Avatar
+          key={i}
+          onClick={this.handleOpen}
+          alt={i + avatar.friend}
+          src={avatar.picture}
+          className={classes.avatar}
+        />
+      );
+    });
 
     return (
-      <div className={classes.root}>
-        <Paper square elevation={0} className={classes.header}>
-          <Typography>{tutorialSteps[activeStep].label}</Typography>
-        </Paper>
-        <img
-          className={classes.img}
-          src={tutorialSteps[activeStep].imgPath}
-          alt={tutorialSteps[activeStep].label}
-        />
-        <MobileStepper
-          steps={maxSteps}
-          position="static"
-          activeStep={activeStep}
-          className={classes.mobileStepper}
-          nextButton={
-            <Button
-              size="small"
-              onClick={this.handleNext}
-              disabled={activeStep === maxSteps - 1}
-            >
-              Next
-              {theme.direction === "rtl" ? (
-                <KeyboardArrowLeft />
-              ) : (
-                <KeyboardArrowRight />
-              )}
-            </Button>
-          }
-          backButton={
-            <Button
-              size="small"
-              onClick={this.handleBack}
-              disabled={activeStep === 0}
-            >
-              {theme.direction === "rtl" ? (
-                <KeyboardArrowRight />
-              ) : (
-                <KeyboardArrowLeft />
-              )}
-              Back
-            </Button>
-          }
-        />
+      <div>
+        <Typography gutterBottom>Checkout Stories!</Typography>
+        <div className={classes.row}>{mappedAvatars}</div>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={this.state.open}
+          onClose={this.handleClose}
+        >
+          <div style={getModalStyle()} className={classes.paper}>
+            <Typography variant="title" id="modal-title">
+              Text in a modal
+            </Typography>
+            <TextMobileStepper mappedStories={mappedStories} />
+          </div>
+        </Modal>
       </div>
     );
   }
 }
 
-TextMobileStepper.propTypes = {
-  classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired
+SimpleModal.propTypes = {
+  classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles, { withTheme: true })(TextMobileStepper);
+// We need an intermediary variable for handling the recursive nesting.
+const SimpleModalWrapped = withStyles(styles)(SimpleModal);
+
+export default SimpleModalWrapped;
